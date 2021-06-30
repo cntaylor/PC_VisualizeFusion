52f25a6ef1fba5deec9b5d1aea92065d9e4f1bb6
Clark Taylor <clark.n.taylor@gmail.com>
2021-06-30 15:56:15 -0400

    Added graphing stuff for understanding

--- main.py
+++ main.py
@@ -26,21 +26,35 @@ def binary_search(theta, lo = constant.K_INIT, hi = 1):
     #     if
 
     # print(generate_C_c_inv(hi, theta))
-    print('mah distance',formulas.mahalanobis_distance(generate_C_c_inv(hi*.99, theta)))
+    print('theta',theta,'mah distance',formulas.mahalanobis_distance(generate_C_c_inv(hi*.99, theta)))
     # print(formulas.mahalanobis_distance(generate_C_c_inv(lo, theta)))
     # print(chi2.ppf((1 - 0.05), df=2))
  
     # C_ac = LA.inv(LA.inv(constant.C_A) - LA.inv(generate_C_c(lo, theta)))
-    tools.plot_ellipse(generate_C_c_inv(hi, theta), ax, color_def='red')
+    C_c_inv = generate_C_c_inv(hi*.99, theta)
+    ax = plt.gca()
+    tools.plot_ellipse(C_c_inv, ax,color_def='red', alpha_val=1)
+    tools.plot_ellipse(constant.C_A_INV,ax,color_def='blue', alpha_val=1)
+    tools.plot_ellipse(constant.C_B_INV,ax,color_def='blue', alpha_val=1)
+    print ('C_ac_inv is',constant.C_A_INV-C_c_inv)
+    C_ac = LA.inv(constant.C_A_INV-C_c_inv)
+    tools.plot_ellipse(C_ac,ax,color_def='green', alpha_val=1)
+    print ('C_bc_inv is',constant.C_B_INV-C_c_inv)
+    C_bc = LA.inv(constant.C_B_INV-C_c_inv)
+    tools.plot_ellipse(C_bc,ax,color_def='green', alpha_val=1)
+    plt.title(f'theta of {theta}')
+    plt.show()
+    
     # tools.plot_ellipse(C_ac, ax, color_def='green')
+    # plt.show()
 
-    # k_s = np.linspace(lo, hi)
-    # maha = []
-    # for k in k_s:
-    #     maha.append(formulas.mahalanobis_distance(generate_C_c_inv(k, theta)))
+    k_s = np.linspace(0, .99*hi)
+    maha = []
+    for k in k_s:
+        maha.append(formulas.mahalanobis_distance(generate_C_c_inv(k, theta)))
 
-    # ax.plot(k_s, maha)
-    # plt.show()    
+    plt.plot(k_s, maha)
+    plt.show()    
 
 
 
